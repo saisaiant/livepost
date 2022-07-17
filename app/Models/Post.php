4acecff1fb4f409c724dceb7ca394d394bfb45fe
kosai <saisaiant@gmail.com>
2022-07-17 13:09:02 +0630

    Add Relationship 1to1 1toMany

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Foundation\Auth\User;
 
 class Post extends Model
 {
@@ -12,4 +13,24 @@ class Post extends Model
     protected $casts = [
         'body' => 'array'
     ];
+
+    public function getTitleUpperCaseAttribute()
+    {
+        return strtoupper($this->title);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = strtolower($value);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'post_user', 'post_id', 'user_id');
+    }
 }
